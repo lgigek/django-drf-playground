a7b271ba20e1e8a15bbd8a5e298e7ddcd3a34167
lgigek <lucasgigek@gmail.com>
2021-11-27 13:47:24 -0300

    created models and added them to admin

--- django_drf_playground/apps/to_do_list/admin.py
+++ django_drf_playground/apps/to_do_list/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from django_drf_playground.apps.to_do_list.models import Task
+from django_drf_playground.apps.to_do_list.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ("id", "name")
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["title", "status", "user"]
+    search_fields = ("id", "title", "user", "status")
--- django_drf_playground/apps/to_do_list/models.py
+++ django_drf_playground/apps/to_do_list/models.py
@@ -1,3 +1,38 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+from model_utils import Choices
+
+
+class StandardModelMixin(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class User(StandardModelMixin):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(StandardModelMixin):
+    STATUS = Choices(
+        # 1st value is what is saved on db, 2nd value is what is used on code, 3rd value is what displays on admin
+        ("to_do", "TO_DO", "To do"),
+        ("in_progress", "IN_PROGRESS", "In progress"),
+        ("done", "DONE", "done"),
+    )
+
+    title = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+    status = models.CharField(max_length=50, choices=STATUS)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
