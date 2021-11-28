85118b52d2aa6cf0836f8bd49b013f270ccf8a03
lgigek <lucasgigek@gmail.com>
2021-11-28 15:37:47 -0300

    created some tests for POST route

--- tests/integration/apps/to_do_list/drf/v1/views/test_users_route.py
+++ tests/integration/apps/to_do_list/drf/v1/views/test_users_route.py
@@ -5,7 +5,10 @@ import pytest
 from rest_framework.response import Response
 from rest_framework.test import APIClient
 
+from django_drf_playground.apps.to_do_list.models import User
 from tests.support.utils import create_user
+
+ROUTE_URI = "/api/v1/users"
 
 
 @pytest.mark.django_db
@@ -26,7 +29,7 @@ class TestRetrieve:
         create_user()
         create_user()
 
-        response: Response = api_client.get(f"/api/v1/users/{uuid.uuid4()}")
+        response: Response = api_client.get(f"{ROUTE_URI}/{uuid.uuid4()}")
 
         assert response.status_code == 404
 
@@ -37,7 +40,7 @@ class TestList:
         user_1 = create_user()
         user_2 = create_user()
 
-        response: Response = api_client.get(f"/api/v1/users")
+        response: Response = api_client.get(ROUTE_URI)
 
         assert response.status_code == 200
         assert len(response.json()) == 2
@@ -45,3 +48,40 @@ class TestList:
         user_ids = [user["id"] for user in response.json()]
         assert str(user_1.id) in user_ids
         assert str(user_2.id) in user_ids
+
+
+@pytest.mark.django_db
+class TestCreate:
+    def test_should_return_201_and_return_created_user(self, api_client: APIClient):
+
+        request_data = {"name": "Gigek", "identifier": "some_cool_and_unique_identifier"}
+
+        response: Response = api_client.post(ROUTE_URI, request_data, format="json")
+
+        assert response.status_code == 201
+        assert User.objects.all().count() == 1
+
+        created_user = User.objects.get()
+        assert str(created_user.id) == response.json()["id"]
+        assert created_user.name == request_data["name"]
+        assert created_user.identifier == request_data["identifier"]
+
+    def test_should_return_400_for_invalid_body(self, api_client: APIClient):
+
+        request_data = {"invalid": "body"}
+
+        response: Response = api_client.post(ROUTE_URI, request_data, format="json")
+
+        assert response.status_code == 400
+        assert User.objects.all().count() == 0
+
+    def test_should_return_400_for_duplicated_identifier(self, api_client: APIClient):
+
+        duplicated_identifier = "xpto"
+        create_user(identifier=duplicated_identifier)
+        request_data = {"name": "Gigek", "identifier": duplicated_identifier}
+
+        response: Response = api_client.post(ROUTE_URI, request_data, format="json")
+
+        assert response.status_code == 400
+        assert User.objects.all().count() == 1  # from user previously created
